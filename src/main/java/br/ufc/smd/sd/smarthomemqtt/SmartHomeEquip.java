977804be74feecf481d0e6f22a7e5268d87e1846
Edvardo <Edvardo@192.168.0.14>
2019-06-08 16:34:10 -0300

    Versão 03 - Criados equipamentos de TV, Sistema de Som e Lampada

--- src/main/java/br/ufc/smd/sd/smarthomemqtt/SmartHomeEquip.java
+++ src/main/java/br/ufc/smd/sd/smarthomemqtt/SmartHomeEquip.java
@@ -10,70 +10,32 @@ package br.ufc.smd.sd.smarthomemqtt;
  * @author Edvardo e Igor
  */
 public class SmartHomeEquip {
+    
     String idEquip;
     String status;
-    String valor;
-
-    public SmartHomeEquip(String idEquip) {
-        this.idEquip = idEquip;
-    } //fim de construtor
-
+   
     public SmartHomeEquip(String idEquip, String status) {
         this.idEquip = idEquip;
         this.status = status;
     } //fim do contrutor2
     
-    public SmartHomeEquip(String idEquip, String status, String valor) {
-        this.idEquip = idEquip;
-        this.status = status;
-        this.valor = valor;
-    } //fim de construtor 3
-
     void executar(String msg) {
-        if ("lamp01".equals(this.idEquip)){
-            switch (msg) {
-                case "ligar":
-                    this.status = "ligado";
-                    this.valor = "100%";
-                    break;
-                case "desligar":
-                    this.status = "desligado";
-                    this.valor = "0%";
-                    break;
-                case "meia-luz":
-                    this.status = "ligado em meia luz";
-                    this.valor = "50%";
-                    break;
-                default:
-                    this.status = "deu merda";
-                    this.valor = "lascou";
-                    break;
-            }
-            
+        if (null == msg){
+            this.status = "sem mensagem";
+        } else switch (msg) {
+            case "ligar":
+                this.status = "Ligado";
+                break;
+            case "desligar":
+                this.status = "desligado";
+                break;
+            default:
+                this.status = "deu merda";
+                break;
         }
-        if ("lamp02".equals(this.idEquip)){
-            switch (msg) {
-                case "ligar":
-                    this.status = "ligado";
-                    this.valor = "100%";
-                    break;
-                case "desligar":
-                    this.status = "desligado";
-                    this.valor = "0%";
-                    break;
-                case "meia-luz":
-                    this.status = "ligado em meia luz";
-                    this.valor = "50%";
-                    break;
-                default:
-                    this.status = "deu merda";
-                    this.valor = "lascou";
-                    break;
-            }
-            
-        }
-        System.out.println(msg);
-        System.out.println(this.idEquip + ": " + this.status + " - " + this.valor);
+        
+        System.out.println(this.idEquip + ": " + this.status);
+        System.out.println();
     }
     
     
